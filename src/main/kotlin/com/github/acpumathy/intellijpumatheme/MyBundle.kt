d7be9fd9d20b7d463d61160e9916ae95b6f1a9da
GitHub Action <action@github.com>
2024-05-02 16:03:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/acpumathy/intellijpumatheme/MyBundle.kt
+++ src/main/kotlin/com/github/acpumathy/intellijpumatheme/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.acpumathy.intellijpumatheme
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/acpumathy/intellijpumatheme/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/acpumathy/intellijpumatheme/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.acpumathy.intellijpumatheme.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
